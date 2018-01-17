bd6c62a8c66a5628fd71b63499646105b631998a
SultanMashukov <mashukov.s@lema.agency>
2018-01-17 15:51:10 +0300

    In iblock "tours-in-russia" added new property "ELEM_HEAD_PICTURE". For tours-in-russia elements added changeable head picture

--- tours-in-russia/detail.php
+++ tours-in-russia/detail.php
@@ -1,7 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 ?>
-    <div class="head-img head-img_transport">
+    <div class="head-img head-img_transport" <?$APPLICATION->ShowViewContent('head_pic')?>>
         <div class="lad-slideshow__block-title">
             <h1 class="lad-slideshow__block-title__h1"><?=$APPLICATION->ShowTitle(false);?></h1>
         </div>
