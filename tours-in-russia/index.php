7827bc70b7d8803b810ff0f0a4ab0177d0a7adaa
SultanMashukov <mashukov.s@lema.agency>
2018-01-17 15:29:18 +0300

    On "tours-in-russia" page added photo.detail component to change the picture in head

--- tours-in-russia/index.php
+++ tours-in-russia/index.php
@@ -2,12 +2,53 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Туры по России");
 ?>
-    <div class="head-img head-img_transport">
+    <div class="head-img head-img_transport" <?$APPLICATION->ShowViewContent('head_pic')?>>
         <div class="lad-slideshow__block-title">
             <h1 class="lad-slideshow__block-title__h1"><?=$APPLICATION->ShowTitle(false);?></h1>
         </div>
     </div>
-
+<?$APPLICATION->IncludeComponent(
+    "bitrix:photo.detail",
+    "pages_head_picture",
+    array(
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "BROWSER_TITLE" => "-",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "DETAIL_URL" => "",
+        "ELEMENT_CODE" => "",
+        "ELEMENT_ID" => "110",
+        "ELEMENT_SORT_FIELD" => "sort",
+        "ELEMENT_SORT_ORDER" => "asc",
+        "FIELD_CODE" => array(
+            0 => "",
+            1 => "",
+        ),
+        "IBLOCK_ID" => "25",
+        "IBLOCK_TYPE" => "content",
+        "MESSAGE_404" => "",
+        "META_DESCRIPTION" => "-",
+        "META_KEYWORDS" => "-",
+        "PROPERTY_CODE" => array(
+            0 => "",
+            1 => "",
+        ),
+        "SECTION_CODE" => "",
+        "SECTION_ID" => "",
+        "SECTION_URL" => "",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SHOW_404" => "N",
+        "COMPONENT_TEMPLATE" => "pages_head_picture"
+    ),
+    false
+);?>
 <? $APPLICATION->IncludeComponent(
     "bitrix:breadcrumb",
     "",
