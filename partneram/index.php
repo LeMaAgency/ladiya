170ac2533c058d0df25c7386c67c9d890a1835f2
SultanMashukov <mashukov.s@lema.agency>
2018-01-22 11:04:16 +0300

    page "partneram" was change

--- partneram/index.php
+++ partneram/index.php
@@ -2,17 +2,17 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Партнерам");
 ?>
-<div class="text__block__wrap">
-    <div class="text___block__images" <?$APPLICATION->ShowViewContent('head_pic')?>>
-        <div class="container">
-            <span class="text___block__images__title">Партнерам</span>
+    <div class="text__block__wrap">
+        <div class="text___block__images" <?$APPLICATION->ShowViewContent('head_pic')?>>
+            <div class="container">
+                <span class="text___block__images__title">Партнерам</span>
+            </div>
         </div>
     </div>
-</div>
-<?$APPLICATION->IncludeComponent(
-	"bitrix:photo.detail", 
-	"pages_head_picture", 
-	array(
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:photo.detail",
+	"pages_head_picture",
+	Array(
 		"AJAX_MODE" => "N",
 		"AJAX_OPTION_ADDITIONAL" => "",
 		"AJAX_OPTION_HISTORY" => "N",
@@ -22,52 +22,40 @@ $APPLICATION->SetTitle("Партнерам");
 		"CACHE_GROUPS" => "Y",
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
+		"COMPONENT_TEMPLATE" => "pages_head_picture",
 		"DETAIL_URL" => "",
 		"ELEMENT_CODE" => "",
 		"ELEMENT_ID" => "220",
 		"ELEMENT_SORT_FIELD" => "sort",
 		"ELEMENT_SORT_ORDER" => "asc",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
+		"FIELD_CODE" => array(0=>"",1=>"",),
 		"IBLOCK_ID" => "25",
 		"IBLOCK_TYPE" => "content",
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
-		"PROPERTY_CODE" => array(
-			0 => "",
-			1 => "",
-		),
+		"PROPERTY_CODE" => array(0=>"",1=>"",),
 		"SECTION_CODE" => "",
 		"SECTION_ID" => "",
 		"SECTION_URL" => "",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"COMPONENT_TEMPLATE" => "pages_head_picture"
-	),
-	false
+		"SHOW_404" => "N"
+	)
+);?> <?$APPLICATION->IncludeComponent(
+	"bitrix:breadcrumb",
+	".default",
+	Array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "",
+		"SITE_ID" => "s1",
+		"START_FROM" => "0"
+	)
 );?>
-
-<?$APPLICATION->IncludeComponent(
-    "bitrix:breadcrumb",
-    ".default",
-    array(
-        "PATH" => "",
-        "SITE_ID" => "s1",
-        "START_FROM" => "0",
-        "COMPONENT_TEMPLATE" => ".default"
-    ),
-    false
-);?>
-
 <div class="text__block__wrap">
-    <div class="container">
-        <div class="text__block">
-        </div>
-    </div>
-</div>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+	<div class="container">
+		<div class="text__block">
+		</div>
+	</div>
+</div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
