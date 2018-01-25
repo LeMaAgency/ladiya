1c597e56ee3eea2c5d4cab93d5b96dce6da0d42c
SultanMashukov1 <mashukov.s@lema.agency>
2018-01-25 15:47:51 +0300

    logo_menu template was changed

---
+++ local/templates/main/components/bitrix/menu/logo_menu/result_modifier.php
@@ -0,0 +1,25 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+global $APPLICATION;
+
+    if(CModule::IncludeModule("iblock"))
+        {
+
+$IBLOCK_ID = 16;
+
+$arOrder = Array();
+$arSelect = Array("ID", "NAME", "IBLOCK_ID", "DETAIL_PAGE_URL");
+$arFilter = Array("IBLOCK_ID"=>$IBLOCK_ID, "ACTIVE"=>"Y","SECTION_ID"=>12);
+$res = CIBlockElement::GetList($arOrder, $arFilter, false, false ,$arSelect);
+
+    while($ob = $res->GetNextElement())
+    {
+    $arFields = $ob->GetFields();           
+    $arResult[] = Array( "NAME"=>$arFields['NAME'], "LINK"=>$arFields['DETAIL_PAGE_URL'],"FROM_IBLOCK"=>"Y");
+    
+    }        
+    
+        }    
+
+?>
--- local/templates/main/components/bitrix/menu/logo_menu/template.php
+++ local/templates/main/components/bitrix/menu/logo_menu/template.php
@@ -1,5 +1,4 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-
 <? if (!empty($arResult)): ?>
     <ul class="nav navbar-nav">
         <?
@@ -7,16 +6,23 @@
             if ($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
                 continue;
             ?>
-            <? if ($arItem["SELECTED"]):?>
+            <? if ($arItem["SELECTED"] && $arItem["FROM_IBLOCK"] != "Y"):?>
             <li class="active"><a href="<?= $arItem["LINK"] ?>" ><?= $arItem["TEXT"] ?></a></li>
-        <? else:?>
-            <li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
-        <? endif ?>
+            <? else:?>
+                <?if($arItem["FROM_IBLOCK"] != "Y"):?>
+                    <li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+                <?endif;?>
+            <? endif ?>
 
         <? endforeach ?>
         <li class="dropdown">
             <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Еще</a>
             <ul class="dropdown-menu">
+                <? foreach($arResult as $arItem):?>
+                    <?if($arItem["FROM_IBLOCK"]=="Y"):?>
+                        <li><a href="<?=$arItem["LINK"]?>"><?=$arItem["NAME"]?></a></li>
+                    <?endif?>
+                <?endforeach;?>
                 <? $APPLICATION->IncludeFile(
                     SITE_DIR."include/logo_menu/logo_menu-more.php",
                     array(),
