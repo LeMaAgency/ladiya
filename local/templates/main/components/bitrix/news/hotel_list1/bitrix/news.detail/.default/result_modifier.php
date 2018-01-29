29f6ccc4e13e5e8476cf87198dedf2ad1d3279c6
SultanMashukovH <mashukov.s@lema.agency>
2018-01-29 15:53:50 +0300

    added 2 iblocks : "infrastructure" and "hotel_rooms". iblock "hotels" was changed. Also added result_modifier for news\hotel_list1\bitrix\news.detail\.default template

---
+++ local/templates/main/components/bitrix/news/hotel_list1/bitrix/news.detail/.default/result_modifier.php
@@ -0,0 +1,40 @@
+<?php
+
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+global $APPLICATION;
+
+if(CModule::IncludeModule("iblock"))
+{
+
+    $IBLOCK_ID = 30; //Инфоблок с номерами гостиниц
+
+    //Получение раздела инфоблока, чье польовательское поле UF_HOTEL привязано к гостинице
+    $hotelID = $arResult["ID"];
+    $arOrder = Array();
+    $arSelect = Array("ID", "NAME", "IBLOCK_ID", "UF_HOTEL");
+    $arFilter = Array("IBLOCK_ID"=>$IBLOCK_ID, "ACTIVE"=>"Y","UF_HOTEL" =>$hotelID);
+    $res = CIBlockSection::GetList($arOrder, $arFilter, false, $arSelect,false );
+    while($ob = $res->GetNextElement())
+    {
+        $arFields = $ob->GetFields();
+        $hotelID  =(int)$arFields['ID'];
+
+    }
+
+
+
+    //Получение элементов из этого раздела
+    $arOrder = Array();
+    $arSelect = Array("ID", "NAME", "IBLOCK_ID", "PROPERTY_PRICE","PROPERTY_ALL_ROOM_PRICE","PROPERTY_PRICE_EXTRA_SPACE");
+    $arFilter = Array("IBLOCK_ID"=>$IBLOCK_ID, "ACTIVE"=>"Y","SECTION_ID"=>$hotelID);
+    $res = CIBlockElement::GetList($arOrder, $arFilter, false, false ,$arSelect);
+
+    while($ob = $res->GetNextElement())
+    {
+        $arFields = $ob->GetFields();
+        $arResult["ROOMS"] = Array($arFields);
+
+    }
+    $arResult["ROOMS_COUNT"] = count($arResult["ROOMS"]); //Количество комнат у гостиницы
+}
