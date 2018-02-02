f11ed423e4cd2fed8dd6fe17c2983a2bb6aaef46
LeMa <kopojiq@gmail.com>
2018-02-02 15:41:42 +0300

    menu updates

---
+++ novyy-razdel-test/index.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новый раздел тест");
+?>
+    <div class="inner-page">
+
+        Text here....
+
+    </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
