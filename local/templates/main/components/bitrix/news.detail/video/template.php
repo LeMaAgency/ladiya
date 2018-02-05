b83e317fff0dd4a504e4a48422c923f1ea969565
SultanMashukov <mashukov.s@lema.agency>
2018-02-05 13:31:36 +0300

    on page 'video' detail template was changed

--- local/templates/main/components/bitrix/news.detail/video/template.php
+++ local/templates/main/components/bitrix/news.detail/video/template.php
@@ -11,32 +11,43 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
+use Bitrix\Main\Localization\Loc;
+Loc::loadMessages(__FILE__);
 ?>
-<div class="container">
-    <div class="text__block">
-        <section class="tour-detail_gallery">
-            <div class="row slider">
-                <? foreach ($arResult['PROPERTIES']['LINK_TO_VIDEO']['VALUE'] as $arLinkVideo): ?>
-                    <?$iCodeYoutubeVideo='';?>
-                    <?if(stristr($arLinkVideo,'youtu.be')){
-                        $iCodeYoutubeVideo = str_replace('/','',strrchr($arLinkVideo,'/'));
-                        }
-                    if(stristr($arLinkVideo,'youtube.com')){
-                        $iCodeYoutubeVideo = str_replace('=','',stristr($arLinkVideo,'='));
+<div class="text__block__wrap">
+    <div class="container">
+        <div class="text__block">
+            <div class="catalog__list catalog__list_3">
+                <? foreach ($arResult['PROPERTIES']['LINK_TO_VIDEO']['VALUE'] as $key=>$arLinkVideo): ?>
+                <?$iCodeYoutubeVideo='';?>
+                <?if(stristr($arLinkVideo,'youtu.be')){
+                    $iCodeYoutubeVideo = str_replace('/','',strrchr($arLinkVideo,'/'));
                     }
-                        ?>
-
-                    <div class="col-xs-12 col-sm-6 col-md-4 item">
-                        <a href="<?= $arLinkVideo; ?>" data-fancybox="video">
-                            <?if($iCodeYoutubeVideo !==''){?>
-                                <span style="background-image: url('http://img.youtube.com/vi/<?= $iCodeYoutubeVideo ?>/sddefault.jpg');"></span>
-                            <?}else{?>
-                                <span style="background-image: url('/YouTube.jpg');"></span>
-                            <?}?>
-                        </a>
+                if(stristr($arLinkVideo,'youtube.com')){
+                    $iCodeYoutubeVideo = str_replace('=','',stristr($arLinkVideo,'='));
+                }      
+                $descriptionCode = $key;
+                //$arImg = \CFile::GetPath($arImg);
+                $arImg = !empty($iCodeYoutubeVideo) ? 'http://img.youtube.com/vi/'.$iCodeYoutubeVideo."/sddefault.jpg" : "/YouTube.jpg"; ?>
+                <a href="<?= $arLinkVideo; ?>" data-fancybox="video" data-caption="<?=$arResult['PROPERTIES']['LINK_TO_VIDEO']['DESCRIPTION'][$descriptionCode]?>" class="catalog__list__item">
+                    <div class="catalog__list__item__img" style="background-image: url('<?=$arImg; ?>');">
+                        <div class="catalog__list__item__img__title">
+                            <?=$arResult['PROPERTIES']['LINK_TO_VIDEO']['DESCRIPTION'][$descriptionCode]?>
+                        </div>
+                        <div class="catalog__list__item__img__wrap">
+                            <div class="catalog__list__item__img__wrap__table">
+                                <div class="catalog__list__item__img__wrap__table__cell">
+                                    <div class="catalog__list__item__img__wrap__title">
+                                        ПОСМОТРЕТЬ
+                                    </div>
+                                    <span class="catalog__list__item__img__wrap__text"></span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                <? endforeach; ?>
+                </a>
+                <?endforeach;?>
             </div>
-        </section>
+        </div>
     </div>
 </div>
