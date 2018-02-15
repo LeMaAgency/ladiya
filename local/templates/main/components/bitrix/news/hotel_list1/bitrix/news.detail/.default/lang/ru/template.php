17c5cd5b03151a8dbca28897bed8d4b64d51ce34
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-02-15 12:02:50 +0300

    edit details hotel page - maps address

--- local/templates/main/components/bitrix/news/hotel_list1/bitrix/news.detail/.default/lang/ru/template.php
+++ local/templates/main/components/bitrix/news/hotel_list1/bitrix/news.detail/.default/lang/ru/template.php
@@ -1,5 +1,5 @@
 <?
-$MESS['NEWS_DETAIL_SHOW_IN_MAP'] = ' - показать на карте';
+$MESS['NEWS_DETAIL_SHOW_IN_MAP'] = ' - <i class="fa fa-map-marker" aria-hidden="true"></i> показать на карте';
 $MESS['NEWS_DETAIL_INFRASTRUKTURA'] = 'Инфраструктура';
 $MESS['NEWS_DETAIL_ROOMS'] = 'Номера';
 
