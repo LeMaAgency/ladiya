39d1fbe5167ac4db1e7c744c49b03010ed936bb3
SultanMashukov <mashukov.s@lema.agency>
2018-02-26 13:10:15 +0300

    on detail video page preview picture fixed

--- local/templates/main/components/bitrix/news.detail/video/result_modifier.php
+++ local/templates/main/components/bitrix/news.detail/video/result_modifier.php
@@ -22,18 +22,14 @@
     //Проверка на доступность картинок с сервера YouTube
     if (!empty($arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"]))
     {
-         foreach ($arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"] as $key=>$prevPic)
+        foreach ($arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"] as $key=>$prevPic)
+        {
+            $pic_status = get_headers($prevPic);
+            if(!strpos($pic_status[0],'200' )) 
             {
-                $pic_status = get_headers($prevPic);
-                if(strpos($pic_status[0],'200' )) 
-                {
-                    echo 'tets';
-                } 
-                else 
-                {
-                   $arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"][$key] = str_replace("sddefault", "0", $arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"][$key]);
-                }
+                $arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"][$key] = str_replace("sddefault", "0", $arResult['PROPERTIES']['LINK_TO_VIDEO']["PREVIEW_PICTURE"][$key]);
             }
+        }
     }
                   
    
