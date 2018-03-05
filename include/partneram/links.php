00df2060d8f3929edaa7ee465b20fbb6c8533da2
SultanMashukovH <mashukov.s@lema.agency>
2018-03-05 16:46:22 +0300

    added folder for document files

--- include/partneram/links.php
+++ include/partneram/links.php
@@ -3,11 +3,10 @@
 	<li><a href="http://www.ladya-kmv.ru/files/images/dogovor_spa___2017.doc">Агентский договор на санаторно-курортные путевки</a><br>
  </li>
 	<li><a href="http://www.ladya-kmv.ru/files/images/docs/zayavka_group.doc">Бланк заявки на групповой тур</a><br>
-	</li>
+ </li>
 	<li><a href="http://www.ladya-kmv.ru/files/images/docs/individ.doc">Бланк заявки на индивидуальный тур</a><br>
-	</li>
+ </li>
 	<li><a href="http://www.ladya-kmv.ru/files/images/docs/zayavka_bron.doc">Бланк заявки на размещение</a><br>
-	</li>
-	<li><a href="http://www.ladya-kmv.ru/files/images/docs/otchet_agenta.doc">Бланк Отчета агента и акта выполненных работ</a><br>
-	</li>
+ </li>
+	<li><a href="http://www.ladya-kmv.ru/files/images/docs/otchet_agenta.doc">Бланк Отчета агента и акта выполненных работ</a></li>
 </ul>
