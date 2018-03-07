62e35f1dae7e049ba6137d8a420b059e523aeeef
SultanMashukovH <mashukov.s@lema.agency>
2018-03-07 18:38:13 +0300

    In footer added small news block ,displaying the last two news on the page

---
+++ local/templates/main/components/bitrix/news.list/footer_news/template.php
@@ -0,0 +1,33 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+if(empty($arResult['ITEMS']))
+    return;
+$strEditLink = \CIBlock::GetArrayByID($arParams['IBLOCK_ID'], 'ELEMENT_EDIT');
+$strDeleteLink = \CIBlock::GetArrayByID($arParams['IBLOCK_ID'], 'ELEMENT_DELETE');
+?>
+    <? foreach($arResult['ITEMS'] as $arItem):
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strEditLink);
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strDeleteLink, $confirmDelete);
+        ?>
+        <li id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+            <div class="row">
+                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                    <img src="<?=$arItem["PREVIEW_PICTURE"]['SRC']?>">
+                </div>
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
+                    <p><?=mb_strimwidth($arItem['PREVIEW_TEXT'], 0, 80, "...");?></p>
+                </div>
+            </div>
+        </li>
+    <? endforeach; ?>
