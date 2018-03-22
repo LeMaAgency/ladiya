a6af5383a170027047008488338ba296efaee8e3
SultanMashukovH <mashukov.s@lema.agency>
2018-03-22 13:10:13 +0300

    For sanatorium and hotel rooms changed price captions

--- local/templates/main/components/bitrix/news/hotel_list1/bitrix/news.detail/.default/lang/ru/template.php
+++ local/templates/main/components/bitrix/news/hotel_list1/bitrix/news.detail/.default/lang/ru/template.php
@@ -3,9 +3,9 @@ $MESS['NEWS_DETAIL_SHOW_IN_MAP'] = ' - <i class="fa fa-map-marker" aria-hidden="
 $MESS['NEWS_DETAIL_INFRASTRUKTURA'] = 'Инфраструктура';
 $MESS['NEWS_DETAIL_ROOMS'] = 'Номера';
 $MESS['NEWS_DETAIL_ROOMS_PRICE_TITLE'] = 'Цена за номер в сутки:';
-$MESS['NEWS_DETAIL_ROOMS_MAIN_PLACE_PRICE'] = 'Основное место в номере';
-$MESS['NEWS_DETAIL_ROOMS_ALL_ROOM_PRICE'] = 'Одноместное размещение в номере';
-$MESS['NEWS_DETAIL_ROOMS_EXTRA_PLACE_PRICE'] = 'Дополнительное место в номере';
+$MESS['NEWS_DETAIL_ROOMS_MAIN_PLACE_PRICE'] = 'Стоимость номера';
+$MESS['NEWS_DETAIL_ROOMS_ALL_ROOM_PRICE'] = 'Одноместное размещение';
+$MESS['NEWS_DETAIL_ROOMS_EXTRA_PLACE_PRICE'] = 'Дополнительное место';
 $MESS['NEWS_DETAIL_ROOMS_DESCRIPTION'] = 'Описание';
 $MESS['NEWS_DETAIL_ROOMS_COMFORT'] = 'Удобства';
 $MESS['NEWS_DETAIL_ROOMS_CURRENCY'] = 'р' ;
