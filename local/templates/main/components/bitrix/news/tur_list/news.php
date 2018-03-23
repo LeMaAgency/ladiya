a3af081e95497772965b84ca4b2873846a028106
SultanMashukovH <mashukov.s@lema.agency>
2018-03-24 00:03:53 +0300

    For tour list added changeable head picture

--- local/templates/main/components/bitrix/news/tur_list/news.php
+++ local/templates/main/components/bitrix/news/tur_list/news.php
@@ -39,48 +39,48 @@ $viewMode = $curView ? 'list' : '';
         <h1 class="lad-slideshow__block-title__h1"><?=$APPLICATION->ShowTitle(false);?></h1>
     </div>
 </div>
-<?/*$APPLICATION->IncludeComponent(
-    "bitrix:photo.detail",
-    "pages_head_picture",
-    array(
-        "AJAX_MODE" => "N",
-        "AJAX_OPTION_ADDITIONAL" => "",
-        "AJAX_OPTION_HISTORY" => "N",
-        "AJAX_OPTION_JUMP" => "N",
-        "AJAX_OPTION_STYLE" => "Y",
-        "BROWSER_TITLE" => "-",
-        "CACHE_GROUPS" => "Y",
-        "CACHE_TIME" => "36000000",
-        "CACHE_TYPE" => "A",
-        "DETAIL_URL" => "",
-        "ELEMENT_CODE" => "",
-        "ELEMENT_ID" => "110",
-        "ELEMENT_SORT_FIELD" => "sort",
-        "ELEMENT_SORT_ORDER" => "asc",
-        "FIELD_CODE" => array(
-            0 => "",
-            1 => "",
-        ),
-        "IBLOCK_ID" => "25",
-        "IBLOCK_TYPE" => "content",
-        "MESSAGE_404" => "",
-        "META_DESCRIPTION" => "-",
-        "META_KEYWORDS" => "-",
-        "PROPERTY_CODE" => array(
-            0 => "",
-            1 => "",
-        ),
-        "SECTION_CODE" => "",
-        "SECTION_ID" => "",
-        "SECTION_URL" => "",
-        "SET_LAST_MODIFIED" => "N",
-        "SET_STATUS_404" => "N",
-        "SET_TITLE" => "N",
-        "SHOW_404" => "N",
-        "COMPONENT_TEMPLATE" => "pages_head_picture"
-    ),
-    false
-);*/?>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:photo.detail", 
+	"pages_head_picture", 
+	array(
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"DETAIL_URL" => "",
+		"ELEMENT_CODE" => "",
+		"ELEMENT_ID" => "",
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"IBLOCK_ID" => "25",
+		"IBLOCK_TYPE" => "content",
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SECTION_CODE" => "",
+		"SECTION_ID" => "",
+		"SECTION_URL" => "",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"COMPONENT_TEMPLATE" => "pages_head_picture"
+	),
+	false
+);?>
 <?$APPLICATION->IncludeComponent(
     "bitrix:breadcrumb",
     ".default",
